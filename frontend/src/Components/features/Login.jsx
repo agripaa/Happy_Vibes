152651f0fe2394c7605deb4e42e0fd56fedd046a
BangOns <syahr9950@gmail.com>
2023-06-14 01:10:34 +0700

    responsive to tablet and desktop

--- frontend/src/Components/features/Login.jsx
+++ frontend/src/Components/features/Login.jsx
@@ -14,72 +14,74 @@ function Login() {
           </figure>
         </div>
         <div className="ContainerLogin-Form-SignIn">
-          <header className="JudulLogin">
-            <h1 className=" heading-bold">Let’s Sign you In</h1>
-          </header>
-          <section className="myFormLogin">
-            <form>
-              <div className="Container-Email-Login">
-                <label>Email</label>
-                <input
-                  type="email"
-                  placeholder="example123@gmail.com"
-                  required
-                  name=""
-                  id=""
-                />
-                <div className="errorLogin1">
-                  {/* <p>Email Tidak Sesuai !!</p> */}
-                </div>
-              </div>
-              <div className="Container-Password-Login">
-                <label className="labelPasswordLogin">Password</label>
-                <input
-                  type="password"
-                  placeholder="****"
-                  required
-                  name=""
-                  id=""
-                />
-                <div className="errorLogin2">
-                  {/* <p>password salah !!</p> */}
-                </div>
-                <div className="optionsLogin">
-                  <div className="optionsLogin-remember">
-                    <input type="checkbox" name="" id="" />
-                    <label className="color-neutral-60">Remember Me</label>
-                  </div>
-                  <div className="optionsLogin-forgot">
-                    <Link to={"/"} className="color-neutral-60">
-                      Forgot Password
-                    </Link>
+          <div className="WrapperLogin">
+            <header className="JudulLogin">
+              <h1 className=" heading-bold">Let’s Sign you In</h1>
+            </header>
+            <section className="myFormLogin">
+              <form>
+                <div className="Container-Email-Login">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    placeholder="example123@gmail.com"
+                    required
+                    name=""
+                    id=""
+                  />
+                  <div className="errorLogin1">
+                    {/* <p>Email Tidak Sesuai !!</p> */}
                   </div>
                 </div>
-              </div>
-              <div className="buttonLogin">
-                <button type="submit" className="bcolor-primary-30">
-                  Login
+                <div className="Container-Password-Login">
+                  <label className="labelPasswordLogin">Password</label>
+                  <input
+                    type="password"
+                    placeholder="****"
+                    required
+                    name=""
+                    id=""
+                  />
+                  <div className="errorLogin2">
+                    {/* <p>password salah !!</p> */}
+                  </div>
+                  <div className="optionsLogin">
+                    <div className="optionsLogin-remember">
+                      <input type="checkbox" name="" id="" />
+                      <label className="color-neutral-60">Remember Me</label>
+                    </div>
+                    <div className="optionsLogin-forgot">
+                      <Link to={"/"} className="color-neutral-60">
+                        Forgot Password
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="buttonLogin">
+                  <button type="submit" className="bcolor-primary-30">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </section>
+            <div className="orLogin">
+              <p>Or</p>
+            </div>
+            <section className="ContainerLogin2">
+              <div className="Button-Login-google">
+                <button>
+                  Login With Google
+                  <img src={GoogleLogin} alt="" />
                 </button>
               </div>
-            </form>
-          </section>
-          <div className="orLogin">
-            <p>Or</p>
+              <div className="ContainerLogin2-Register">
+                <p>
+                  Don’t have an account?
+                  <Link to={"/register"}>Register now</Link>
+                </p>
+              </div>
+            </section>
           </div>
-          <section className="ContainerLogin2">
-            <div className="Button-Login-google">
-              <button>
-                Login With Google
-                <img src={GoogleLogin} alt="" />
-              </button>
-            </div>
-            <div className="ContainerLogin2-Register">
-              <p>
-                Don’t have an account?
-                <Link to={"/register"}>Register now</Link>
-              </p>
-            </div>
-          </section>
         </div>
       </div>
     </div>
--- frontend/src/Components/features/Register.jsx
+++ frontend/src/Components/features/Register.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
-  return <div>Register</div>;
+  return <div></div>;
 }
 
 export default Register;
