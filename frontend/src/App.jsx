1fc3854bf7f12bd2a370f153529c6159f460260c
BangOns <syahr9950@gmail.com>
2023-06-16 16:48:39 +0700

    Components Navbar inDesktop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./Components/features/Home";
