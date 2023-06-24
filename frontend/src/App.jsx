634eb04835b476ce1297e08fd9ad2f9afae1af30
BangOns <syahr9950@gmail.com>
2023-06-24 17:46:25 +0700

    Clear to Page Forgot Password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import Login from "./Components/features/Login";
 import Register from "./Components/features/Register";
 import OTP from "./Components/features/OTP";
 import EmailAuth from "./Components/features/EmailAuth";
+import ForgotPassword from "./Components/features/ForgotPassword";
+import RenewPassword from "./Components/features/RenewPassword";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/authOtp" element={<EmailAuth />} />
         <Route path="/authOtp/otp" element={<OTP />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+        <Route path="/forgot/newpw" element={<RenewPassword />} />
       </Routes>
     </>
   );
