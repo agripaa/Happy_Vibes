2258ff32159da943481bdc558b223facf34bc589
Jounnyy <agrieva.xananda08@gmail.com>
2024-03-07 20:59:11 +0700

    [Done] ~ upload story with image

--- Controller/stories.controller.js
+++ Controller/stories.controller.js
@@ -1,6 +1,7 @@
 const CategoryStories = require('../Models/categoryStoriesData.model.js');
 const ImageStories = require('../Models/imageStoriesData.model.js');
 const Stories = require('../Models/storiesData.model.js');
+const path = require('path');
 
 module.exports = {
     getAll: async function(req, res) {
@@ -11,6 +12,46 @@ module.exports = {
         } catch (error) {
             console.error(error);
             res.status(500).json({status: 500, msg: error.message});
+        }
+    },
+    handleImageUpload: async function(files, text_stories, stories, res){
+        try {
+            const file = files.file;
+            const { data, name } = file;
+            const size = data.length;
+            const extend = path.extname(name);
+            const name_img = file.md5 + extend;
+            const url = `/stories/${name_img}`;
+    
+            if (size > 5000000) {
+                return res.status(422).json({ status: 422, msg: "Images must be less than 5MB" });
+            }
+    
+            file.mv(`./public/stories/${name_img}`, err => {
+                if (err) {
+                    return res.status(500).json({ status: 500, msg: 'Internal server error', image: "Image hasn't been uploaded!", error: err });
+                }
+            });
+    
+            const image_stories = await ImageStories.create({
+                url_stories: url,
+                name_image: name_img,
+                text_stories,
+                stories_id: stories.id
+            });
+            
+            return res.status(200).json({ status: 200, msg: "Story uploaded successfully!", datas_story: {image_stories, stories} });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: 500, msg: error.message });
+        }
+    },
+    handleTextUpload: async function (text_stories, stories, res){
+        try {
+            
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ status: 500, msg: error.message });
         }
     },
     uploadStories: async function(req, res) {
@@ -30,7 +71,7 @@ module.exports = {
             });
 
             if (category_stories['category_story'] === "IMAGE") {
-                handleImageUpload(files, text_stories, stories, res);
+                module.exports.handleImageUpload(files, text_stories, stories, res);
             } else if (category_stories['category_story'] === "TEXT") {
                 handleTextUpload(text_stories, stories, res);
             } else {
@@ -41,44 +82,4 @@ module.exports = {
             res.status(500).json({ status: 500, msg: error.message });
         }
     },
-    handleImageUpload: async function(files, text_stories, stories, res){
-        try {
-            const file = files.file;
-            const { data, name } = file;
-            const size = data.length;
-            const extend = path.extname(name);
-            const name_img = file.md5 + extend;
-            const url = `/stories/${name_img}`;
-    
-            if (size > 5000000) {
-                return res.status(422).json({ status: 422, msg: "Images must be less than 5MB" });
-            }
-    
-            file.mv(`./public/postings/${name_img}`, err => {
-                if (err) {
-                    return res.status(500).json({ status: 500, msg: 'Internal server error', image: "Image hasn't been uploaded!", error: err });
-                }
-            });
-    
-            const image_stories = await ImageStories.create({
-                url_stories: url,
-                name_image: name_img,
-                text_stories,
-                stories_id: stories.id
-            });
-    
-            return res.status(200).json({ status: 200, msg: "Story uploaded successfully!", image_stories, stories });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ status: 500, msg: error.message });
-        }
-    },
-    handleTextUpload: async function (text_stories, stories, res){
-        try {
-            
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ status: 500, msg: error.message });
-        }
-    }
 }
--- Routes/stories.route.js
+++ Routes/stories.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll } = require('../Controller/stories.controller.js');
+const { getAll, uploadStories } = require('../Controller/stories.controller.js');
+const { sessionUser } = require('../middleware/session.js');
 
 router.get('/', getAll);
+router.post('/new/upload/', uploadStories)
 
 module.exports = router;
