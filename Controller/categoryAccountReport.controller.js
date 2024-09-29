4d7bd2a1b1926c56ac7a283b310346771f0afc4e
agripaa <agrieva.xananda08@gmail.com>
2024-09-29 19:09:34 +0700

    add new function

--- Controller/categoryAccountReport.controller.js
+++ Controller/categoryAccountReport.controller.js
@@ -7,7 +7,17 @@ module.exports = {
             res.status(200).json({ status: 200, data: category_report });
         } catch (error) {
             console.error(error);
-            res.status(500).json({status: 500, msg: error.message});
+            res.status(500).json({status: 500, msg: "Internal server error!"});
+        }
+    },
+    createCategoryUserReport: async function(req, res){
+        const { category_report, desc_category_report } = req.body;
+        
+        try {
+            
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({status: 500, msg: "Internal server error!"});
         }
     }
 }                                                                                                                                                                                                                       
